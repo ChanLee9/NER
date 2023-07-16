4cc8f1bb130a7fc09c5ba063753f458d98838855
chan <17612872708@163.com>
2023-07-16 15:27:25 +0800

    sa

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-print(11)
+import os
+import json
+import pandas as pd
 
-print(22)
+label_path = "data/label.txt"
+
+labels = pd.read_csv(label_path)
+
+def cus_fun(x):
+    id = x["id"]
+    res = {
+                "entities": [],
+                "starts": [],
+                "ends": []
+            }
+    return res
+
+# print(labels)
+labels["test"] = labels.apply(lambda x:cus_fun(x), axis=1)
+
+print(labels)
