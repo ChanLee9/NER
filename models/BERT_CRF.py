58f9f1d200a8d84b025c5fc3884388080cfd2919
chan <17612872708@163.com>
2023-07-16 17:15:40 +0800

    ss

--- models/BERT_CRF.py
+++ models/BERT_CRF.py
@@ -1,7 +1,91 @@
 import torch
 import torch.nn as nn
 from transformers import BertModel
-from torchcrf import CRF
+# from torchcrf import CRF
+
+class CRF(object):
+    def __init__(self, num_tags, batch_first=True):
+        self.num_tags = num_tags
+        self.batch_first = batch_first
+        self.transitions = nn.Parameter(torch.Tensor(num_tags, num_tags))
+        self.start_transitions = nn.Parameter(torch.Tensor(num_tags))
+        self.end_transitions = nn.Parameter(torch.Tensor(num_tags))
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.transitions)
+        nn.init.normal_(self.start_transitions)
+        nn.init.normal_(self.end_transitions)
+    
+    def forward(self, emissions, tags, mask):
+        if self.batch_first:
+            emissions = emissions.transpose(0, 1)
+            tags = tags.transpose(0, 1)
+            mask = mask.transpose(0, 1)
+        
+        scores = self._compute_scores(emissions, tags, mask)
+        partition = self._compute_log_partition(emissions, mask)
+        return torch.mean(partition - scores)
+
+    def decode(self, emissions, mask):
+        if self.batch_first:
+            emissions = emissions.transpose(0, 1)
+            mask = mask.transpose(0, 1)
+        
+        return self._viterbi_decode(emissions, mask)
+    
+    def _viterbi_decode(self, emissions, mask):
+        seq_length = emissions.shape[0]
+        mask = mask.float()
+        # mask: (seq_length, batch_size)
+        # emissions: (seq_length, batch_size, num_tags)
+        transitions = self.transitions.unsqueeze(0)
+        # transitions: (1, num_tags, num_tags)
+        # emissions: (seq_length, batch_size, num_tags)
+        scores = self.start_transitions + emissions[0]
+        # scores: (batch_size, num_tags)
+        paths = []
+        for i in range(1, seq_length):
+            # scores: (batch_size, num_tags, 1)
+            # transitions: (1, num_tags, num_tags)
+            # emissions[i]: (batch_size, num_tags)
+            scores = scores.unsqueeze(2) + transitions + emissions[i].unsqueeze(1)
+            # scores: (batch_size, num_tags, num_tags)
+            scores, paths = torch.max(scores, dim=1)
+            # scores: (batch_size, num_tags)
+            # paths: (batch_size, num_tags)
+            scores = scores.squeeze(1) * mask[i] + scores.squeeze(1) * (1 - mask[i])
+            # scores: (batch_size, num_tags)
+            paths = paths * mask[i].long() + paths * (1 - mask[i]).long()
+            # paths: (batch_size, num_tags)
+        scores = scores + self.end_transitions
+        # scores: (batch_size, num_tags)
+        best_score, best_path = torch.max(scores, dim=1)
+        # best_score: (batch_size)
+        # best_path: (batch_size)
+        return best_path.tolist()
+    
+    def _compute_scores(self, emissions, tags, mask):
+        seq_length = emissions.shape[0]
+        mask = mask.float()
+        # mask: (seq_length, batch_size)
+        # emissions: (seq_length, batch_size, num_tags)
+        scores = self.start_transitions + emissions[0]
+        # scores: (batch_size, num_tags)
+        for i in range(1, seq_length):
+            # scores: (batch_size, num_tags, 1)
+            # transitions: (1, num_tags, num_tags)
+            # emissions[i]: (batch_size, num_tags)
+            scores = scores.unsqueeze(2) + self.transitions + emissions[i].unsqueeze(1)
+            # scores: (batch_size, num_tags, num_tags)
+            new_scores = torch.logsumexp(scores, dim=1)
+            # new_scores: (batch_size, num_tags)
+            new_scores = new_scores * mask[i] + scores.squeeze(1) * (1 - mask[i])
+            # new_scores: (batch_size, num_tags)
+            scores = new_scores
+        scores = scores + self.end_transitions
+        # scores: (batch_size, num_tags)
+        return torch.logsumexp(scores, dim=1)
 
 
 class Model(nn.Module):
